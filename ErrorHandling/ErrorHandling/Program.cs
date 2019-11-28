5c2007ad5ed37209a32d1dfb54a394b5c407632f
Bjorn van Olmen <bjorn.vanolmen@boschrexroth.nl>
2019-11-28 16:30:10 +0100

    Catch try final method

---
+++ ErrorHandling/ErrorHandling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ErrorHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Please enter a number!");
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch()
+            {
+                throw; 
+                Console.WriteLine("Wrong type");
+            }
+
+  
+
+            Console.ReadKey();
+        }
+    }
+}
