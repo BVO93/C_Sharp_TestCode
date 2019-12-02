f5f8ea8cb2d507e50619019f46e9c91114c18f3a
Bjorn van Olmen <bjorn.vanolmen@boschrexroth.nl>
2019-12-02 17:09:14 +0100

    Methods added

---
+++ BasicClassStructure/BasicClassStructure/Human.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClassStructure
+{
+    class Human
+    {
+        //member variables 
+        public string firstName;
+        public string lastName;
+        public string eyeColor;
+        public int age;
+
+
+        public Human()
+        {
+            Console.WriteLine("Human made ");
+
+        }
+
+
+        // Parameterized constructor 
+
+            public Human(string firstName, string lastName, string eyeColor, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+
+        }
+
+        //member method
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi I'm {0} {1 } my eyes are {2} and i am  {3} years old ", firstName, lastName, eyeColor, age);
+        }
+
+    }
+}
---
+++ BasicClassStructure/BasicClassStructure/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BasicClassStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create object of class 
+            Human sissy = new Human("Sissy", "Wagner", "Brown", 29);
+            sissy.IntroduceMyself();
+
+            Human basicHuman = new Human();
+            basicHuman.IntroduceMyself();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
