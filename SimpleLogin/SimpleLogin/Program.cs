c59df100ba52dc645e22a317595182a75adf255d
Bjorn van Olmen <bjorn.vanolmen@boschrexroth.nl>
2019-12-02 15:56:59 +0100

    Simple login example added

---
+++ SimpleLogin/SimpleLogin/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SimpleLogin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isAdmin = false;batelfield
+            bool isRegisterred = true;
+            string userName = "";
+
+            Console.WriteLine("Please enter userName");
+            userName = Console.ReadLine();
+
+
+            if (isRegisterred)
+            {
+                Console.WriteLine("Hi registered user ");
+                if(userName != "")
+                {
+                    Console.WriteLine("Hi there , " + userName);
+                    if(userName.Equals("Admin"))
+                    {
+                        Console.WriteLine("Hi there Admin");
+                    }
+                }
+            }
+
+
+        }
+    }
+}
