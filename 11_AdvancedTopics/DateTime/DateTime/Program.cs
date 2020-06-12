c45d3d31b7bc33bb6b50b8a3a731a7666d541198
Bjorn van Olmen <bjorn.vanolmen@boschrexroth.nl>
2020-06-12 16:28:05 +0200

    Regular expressions

---
+++ 11_AdvancedTopics/DateTime/DateTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DateTimeT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime(1993, 10, 28);
+            Console.WriteLine("My birthay is {0}", dateTime);
+
+
+            // Write today
+            Console.WriteLine(DateTime.Today);
+            // Current time 
+            Console.WriteLine(DateTime.Now);
+
+
+        }
+    }
+}
